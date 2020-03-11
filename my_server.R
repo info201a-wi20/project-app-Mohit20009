723084156f4192bfdca93437b559a270514fdd4e
daniel <daniel@DANIELdeMacBook-Air.local>
2020-03-10 18:19:55 -0700

    change control widget to textInput, add some texts

--- my_server.R
+++ my_server.R
@@ -9,31 +9,37 @@ setwd("~/project-app-Mohit20009")
 opioid_df <- read.csv("Multiple Cause of Death, 1999-2018.csv", stringsAsFactors = FALSE)
 state_codes <- read.csv('state_codes.csv', stringsAsFactors = FALSE)
 
+#group the dataset by relevant columns
 opioid_df_long <- opioid_df %>%
   select(State, Year, Multiple.Cause.of.death, Deaths) %>%
   group_by(State, Multiple.Cause.of.death, Year)
 
+#spread data in wide version in order to add Multiple.Cause.of.death into three categories
 opioid_df_wide <- spread(
   opioid_df_long,
   key = Multiple.Cause.of.death,
   value = Deaths
 )
 
+#set NA value in the data to be 0
 opioid_df_wide[is.na(opioid_df_wide)] <- 0
 
+#generate the three categories of drugs and select those three categories and State, Year columns
 q1_plot_df <- opioid_df_wide %>%
   mutate(Heroin = Heroin, `illegal drugs` = `Other and unspecified narcotics`+ `Other synthetic narcotics` + Heroin, 
          `legal drugs` = Methadone + `Other opioids`) %>%
   select(State, Year, Heroin, `illegal drugs`, `legal drugs`)
 
+#Leftjoin the dataset with state codes
 joined_data <- left_join(q1_plot_df, state_codes, by = "State")
 
 
+#A function that takes users' two inputs: "drug category" and "state" and returns a drug death count line graph
 my_line <- function(data, dc.var = "", st.var = "") {
   data <- data %>% 
     select(State, Year, dc.var) %>% 
-    filter(State == st.var)
-  p <- plot_ly(x = data$Year,
+    filter(State == st.var) #select useful columns and filter specific state input by users
+  p <- plot_ly(x = data$Year, 
                y = data[[dc.var]],
                type = 'scatter', mode = 'lines') %>%
     layout(xaxis = list(title = "Year"),
@@ -42,32 +48,32 @@ my_line <- function(data, dc.var = "", st.var = "") {
   return(p)
 }
 
-
+#A function that takes users' two inputs: "drug category" and "year" and returns a U.S drug death count map
 my_map <- function(data, dc.var, yr.var) {
   l <- list(color = toRGB("white"), width = 2)
   g <- list(
-    scope = 'usa',
+    scope = 'usa', 
     projection = list(type = 'albers usa'),
     showlakes = TRUE,
     lakecolor = toRGB('white')
   )
-  var.equation <- paste0('~', dc.var)
+  var.equation <- paste0('~', dc.var) # Make equation for map color / text
   data <- data %>%
-    filter(Year == yr.var)
+    filter(Year == yr.var) #filter specific year input by user
   p <- plot_geo(data, locationmode = 'USA-states') %>%
     add_trace(
       z = data[[dc.var]], text = ~State, locations = ~code,
       color = data[[dc.var]], colors = 'Purples'
     ) %>%
-    colorbar(title = "Number of people") %>%
+    colorbar(title = "Number of people") %>% #set the colorbar
     layout(
       title = paste0(yr.var, " U.S deaths caused by ", dc.var, " map"),
       geo = g
     )
   return(p)
 }
-  
 
+#server function that returns two functions to return the plots
 server <- function(input, output) {
   output$line_graph <- renderPlotly({ 
     return(my_line(q1_plot_df, input$drugcate, input$state))
--- my_ui.R
+++ my_ui.R
@@ -2,59 +2,61 @@ library(shiny)
 library(plotly)
 library("dplyr")
 
+#data 
 opioid_df <- read.csv("Multiple Cause of Death, 1999-2018.csv")
+
+#take out the state names from data frame, for use as users' choices
 all_states <- opioid_df$State
 unique_states <- unique(all_states)
+
+#take out the years from data frame, for use as users' choices
 all_year <- opioid_df$Year
 unique_year <- unique(all_year)
 
-opioid_df <- read.csv("Multiple Cause of Death, 1999-2018.csv")
-all_states <- opioid_df$State
-unique_states <- unique(all_states)
-all_year <- opioid_df$Year
-unique_year <- unique(all_year)
 
-
-trend_graph_panel <- tabPanel("Death count trend graph", fluid = TRUE,
+#line graph panel as first tabPanel
+trend_graph_panel <- tabPanel(h3("Death count trend graph"), fluid = TRUE,
                       sidebarLayout(
                         sidebarPanel(
                           selectInput(
-                            inputId = "drugcate",
+                            inputId = "drugcate", #assign inputId
                             label = "Drug categories",
                             choices = c("Heroin", "legal drugs", "illegal drugs")
                           ),
-                          selectInput(
-                            inputId = "state",
-                            label = "State",
-                            choices = unique_states
+                          textInput(
+                            inputId = "state", #assign inputId
+                            label = "Find a state",
+                            value = ""
                           )
                         ),
                         mainPanel(
-                          plotlyOutput("line_graph")    
+                          plotlyOutput("line_graph") #draw the line graph
                         )  
                       )
                      )
 
-map_panel <- tabPanel("Death count map", fluid = TRUE,
+#map panel as another tabPanel
+map_panel <- tabPanel(h3("Death count map"), fluid = TRUE,
               sidebarLayout(
                 sidebarPanel(
                   selectInput(
-                    inputId = "drugcate2",
+                    inputId = "drugcate2", #assign inputId
                     label = "Drug categories",
                     choices = c("Heroin", "legal drugs", "illegal drugs")
                   ),
                   selectInput(
-                    inputId = "year",
+                    inputId = "year", #assign inputId
                     label = "Year",
                     choices = unique_year
                   )
                 ),
                 mainPanel(
-                  plotlyOutput("map")
+                  plotlyOutput("map") #draw the map
                 )
               )
              )
 
+#The main tabPanel including the two sub-tabPanels and all the texts
 drug_death_count_panel <- tabPanel(
   title = "Drug death count and drug categories",
   titlePanel("What is the drug death count by prescription and illegal drugs from 1999-2018?"),
@@ -74,11 +76,16 @@ drug_death_count_panel <- tabPanel(
     The illegal drugs include 'Other and unspecified narcotics', 'other synthetic narcotics' and 'Heroin'. 
     Heroin, which is a major well-known illegal drug, is also listed seperately as a reference. For instance, 
     'Other opioids' is a drug type, 'legal drugs' is a drug category, while 'Heroin' is both a drug type and category."),
-  p("In order to get a thorough analysis of the question, it is neccessary to show..........."),
+  p("In order to get a thorough analysis of the question, it is neccessary to show the data from the span of both time and 
+    geomtric region. A trending line graph and a map would be good choices to approach this question."),
   h4("Analysis:"),
-  p("The trending graph the map have given a similar result that in general, the death caused by illegal drugs is more unpredictable and therefore harder to control than the legal drugs.")
+  p("The trending line graph shows each state death count caused by each drug category from 1999 to 2018.(In some states,
+    the data is missing for some years) The map shows U.S death count caused by each drug category in specific year. 
+    By viewing both graphs, we can make conclusions about death count by the three categories in different year and differnet 
+    states. For example, we can tell that in 2018, New York State had the highest death number caused by Heroin.")
 )
 
+#Ui with navbarPage
 ui <- navbarPage(
         title = "",
         drug_death_count_panel
