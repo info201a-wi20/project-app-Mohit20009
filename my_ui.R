960882ba3578589375e9912d23710b53bdeea5eb
zhangj85-1836956 <xcscsf@gmail.com>
2020-03-11 20:01:09 -0700

    Adding comments

--- my_ui.R
+++ my_ui.R
@@ -1,18 +1,20 @@
 library("shinyWidgets")
 
 slider_opioid <- sidebarPanel(
-  # Creating the slider for the range from 1999 to 2018
+  # Creating the slider for the time range from 1999 to 2018
   sliderInput(inputId = "year", label = "Year", min = 1999, max = 2018, value = c(1999,2018), sep = "")
 )
 
 main_content_opioid_death <- mainPanel(
-  # A `plotOutput()` element showing the 'plot' output (defined in the server)
+  # Draw the plot and table in opioid_death_panel tab
   tabsetPanel(
     tabPanel("Plot", plotOutput("plot2")), 
     tabPanel("Table", tableOutput("table2"))
     
   )
 )
+
+# Create the tab for question 2
 opioid_death_panel <- tabPanel(
   title = "Death Caused by Opioid Overdose",
   titlePanel("The number of people dead by Overdosing Opioids in the US"),
